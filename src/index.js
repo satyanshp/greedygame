ef510366c8f00da948c107eb616a0a46ef3ed33f
Satyansh Prajapati <satyanshprajapati58@gmail.com>
2022-12-27 00:00:01 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import App from './App';
 
+const store = configureStore({
+  reducer:{},
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
