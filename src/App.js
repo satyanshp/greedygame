a984698a481cb370bf0e941211c61d0c6cca931b
Satyansh Prajapati <satyanshprajapati58@gmail.com>
2022-12-26 23:01:42 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{backgroundColor:'#00000F',width:'5%'}}></div>
+      <div style={{width:'95%'}}>
+        <div>Analytics</div>
+        <div>
+          <button>date</button>
+          <button>setting</button>
+        </div>
+        <div>
+          <div>
+            <div></div>
+            <div>
+              <button></button>
+            </div>
+            <div>
+              <button>cancel</button>
+              <button>apply changes</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <table>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
